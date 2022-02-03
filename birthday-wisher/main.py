a30c8d0c8422541d736a9a53b6d77c23af2a6921
Jiggly Puff <mian.wang.eit@gmail.com>
2022-02-03 07:46:59 -0800

    Add files via upload

---
+++ birthday-wisher/main.py
@@ -0,0 +1,30 @@
+import pandas
+import smtplib
+import datetime as dt
+import random
+
+my_email = "sophiathehuskyprincess@gmail.com"
+password = "DannyTao0516"
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open (file_path) as letter_file:
+        contents = letter_file.read()
+        letter_content = contents.replace("[NAME]", birthday_person["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n {letter_content}"
+        )
+
+
+
